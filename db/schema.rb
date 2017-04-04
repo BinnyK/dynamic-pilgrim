8d704835b441e4d99083d1d31c5277ae10e2559f
marlbones <mdimaanods@gmail.com>
2017-04-04 19:11:09 +1000

    added table to User with wins, losses, points

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170402071443) do
+ActiveRecord::Schema.define(version: 20170404090555) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20170402071443) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "avatar"
+    t.integer  "wins"
+    t.integer  "losses"
+    t.integer  "points"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
