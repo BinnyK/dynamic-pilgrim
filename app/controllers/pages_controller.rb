04552a3f561f60bb78f6f242d16efd07001f5214
BinnyK <BinnyK@users.noreply.github.com>
2017-05-03 15:19:51 +1000

    Add username to User model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   
   def rankings
+  	@users = User.all
   end
 
   def faq
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405125907) do
+ActiveRecord::Schema.define(version: 20170503034255) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at",               null: false
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170405125907) do
     t.integer  "wins"
     t.integer  "losses"
     t.integer  "points"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
