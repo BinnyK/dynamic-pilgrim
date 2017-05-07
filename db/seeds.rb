d3c6d860421bd895e7ea312a98ce4efadb4f5bd9
BinnyK <BinnyK@users.noreply.github.com>
2017-05-07 14:37:10 +1000

    Remove link to profile and delete seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'admin@iamadmin.com', password: 'tabletennis')
