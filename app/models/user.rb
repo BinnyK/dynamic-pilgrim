6b8508729aea667f2156c81f3b6df0ef79a4cc00
BinnyK <BinnyK@users.noreply.github.com>
2017-05-07 20:26:18 +1000

    Add default role of player and add tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,14 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates_uniqueness_of :username
-
+	after_create :assign_default_role
+  
   def admin?
-     has_role?(:admin)
+    has_role?(:admin)
+  end
+
+  def assign_default_role
+    self.add_role(:player) if self.roles.blank?
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,36 +2,64 @@ require 'spec_helper'
 
 describe User do
 
-	it "has a valid factory" do 
-		expect(build(:user)).to be_truthy
+	describe "validations" do
+		it "has a valid factory" do 
+			expect(build(:user)).to be_truthy
+		end
+
+		it "is invalid without a username" do
+			expect(build(:user, username: nil)).to be_invalid
+		end
+
+		it "is invalid without a unique username" do
+			user = create(:user, username: "Freddo")
+			expect(build(:user, username: "Freddo")).to be_invalid
+		end
+
+		it "is invalid without an email" do
+			expect(build(:user, email: nil)).to be_invalid
+		end
+
 	end
 
-	it "is invalid without a username" do
-		expect(build(:user, username: nil)).to be_invalid
-	end
+	describe "default values" do
 
-	it "is invalid without a unique username" do
-		user = create(:user, username: "Freddo")
-		expect(build(:user, username: "Freddo")).to be_invalid
-	end
+		it "has default wins of 0" do
+			user = build(:user)
+			expect(user.wins).to eq(0)
+		end
 
-	it "is invalid without an email" do
-		expect(build(:user, email: nil)).to be_invalid
-	end
+		it "has default losses of 0" do
+			user = build(:user)
+			expect(user.losses).to eq 0
+		end
 
-	it "has default wins of 0" do
-		user = build(:user)
-		expect(user.wins).to eq(0)
-	end
+		it "has default points of 0" do
+			user = build(:user)
+			expect(user.points).to eq 0
+		end
 
-	it "has default losses of 0" do
-		user = build(:user)
-		expect(user.losses).to eq 0
-	end
+		it "has default role of user" do
+			user = create(:user)
+			expect(user.has_role? :player).to be_truthy
+		end
 
-	it "has default points of 0" do
-		user = build(:user)
-		expect(user.points).to eq 0
+		it "is not an admin" do
+			user = create(:user)
+			expect(user.has_role? :admin).to be_falsey
+		end
+
 	end
 
 end
+
+
+
+
+
+
+
+
+
+
+# Type
