28bd09b11c2bb979ff270e37efe09fbdc6cbdf39
BinnyK <BinnyK@users.noreply.github.com>
2017-05-07 20:14:08 +1000

    Add tests for default values for wins, losses, points

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,35 @@ require 'spec_helper'
 describe User do
 
 	it "has a valid factory" do 
-		expect(FactoryGirl.build(:user).save).to be_truthy
+		expect(build(:user)).to be_truthy
 	end
 
 	it "is invalid without a username" do
-		expect(FactoryGirl.build(:user, username: nil).save).to be_falsey
+		expect(build(:user, username: nil)).to be_invalid
 	end
 
 	it "is invalid without a unique username" do
-		user = FactoryGirl.create(:user, username: "Freddo")
-		expect(FactoryGirl.build(:user, username: "Freddo").save).to be_falsey
+		user = create(:user, username: "Freddo")
+		expect(build(:user, username: "Freddo")).to be_invalid
 	end
-	it "is invalid without an email"
+
+	it "is invalid without an email" do
+		expect(build(:user, email: nil)).to be_invalid
+	end
+
+	it "has default wins of 0" do
+		user = build(:user)
+		expect(user.wins).to eq(0)
+	end
+
+	it "has default losses of 0" do
+		user = build(:user)
+		expect(user.losses).to eq 0
+	end
+
+	it "has default points of 0" do
+		user = build(:user)
+		expect(user.points).to eq 0
+	end
 
 end
